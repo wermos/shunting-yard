f5527c97e2fbb9306f6b7f7163bffbfbf36abbc2
wermos <63574588+wermos@users.noreply.github.com>
2021-07-12 02:20:03 +0530

    Fleshed out config_reader.hpp.

--- include/config_reader.hpp
+++ include/config_reader.hpp
@@ -1,13 +1,20 @@
 #include <fstream>
 #include <string>
 
+struct op {
+	char op_char;
+};
+
 class ConfigReader {
 	public:
 		ConfigReader() = default;
 
+		std::unordered_map<op, precedence> read_precendence_list(const std::string& file_name);
+		std::unordered_map<op, assoc> read_assoc_list(const std::string& file_name);
 	private:
 		ifstream m_file;
 		bool file_exists(const std::string& file_name) {
-			
+			m_file("../config/config.txt");
+			return f.good();
 		}
 };
