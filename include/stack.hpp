2242b3525c32630a2ab012ca5504b6ae9fab4245
wermos <63574588+wermos@users.noreply.github.com>
2021-07-12 05:35:46 +0530

    Wrote my stack class.

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,1 +1,25 @@
-class stack {
+template <typename T>
+class stack<T> {
+	public:
+		stack() : m_index {-1};
+
+		T& pop() {
+			if (index == -1) {
+				std::cerr << "Stack underflow: The stack is empty!\n";
+				//we used a newline character instead of std::endl
+				//because we don't need to flush the buffer.
+			} else {
+				return m_data[index--];
+				//first m_data[index] is returned, and then index is
+				//decremented by 1
+			}
+		}
+			
+		void push(const T& data) {
+			m_data.push_back(data);
+		}
+
+	private:
+		std::vector<T&> m_data {10}; //default capacity
+		int m_index;
+};
