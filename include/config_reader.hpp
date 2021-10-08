189498d5b3c39886ae926600cbc408e6e3bd546e
wermos <63574588+wermos@users.noreply.github.com>
2021-10-08 12:19:43 +0530

    Defined the `config_reader` class.

--- include/config_reader.hpp
+++ include/config_reader.hpp
@@ -5,14 +5,31 @@ struct op {
 	char op_char;
 };
 
+enum assoc : unsigned char { //Saving memory by using the smallest (portable) data type
+	                     //possible
+	LASSOC,
+	RASSOC,
+	NASSOC
+};
+
 class config_reader {
 	public:
-		ConfigReader() = default;
+		config_reader() {
+			std::string file_name {"../config/config.txt"};
+			if (!file_exists(file_name)) {
+				std::cerr << "Fatal error: Configuration file not found.\n";
+				std::cerr << "Make sure that a file called \"config.txt\""
+					  << " exists in a directory called \"config\","
+					  << "which in turn must be in the root directory"
+					  << "of the project.\n";
+			} else {
+				m_file {file_name};
+			}
+		}
 
-		std::unordered_map<op, precedence> read_precendence_list(const std::string& file_name);
-		std::unordered_map<op, assoc> read_assoc_list(const std::string& file_name);
+		auto read_config();
 	private:
-		ifstream m_file;
+		ifstream m_file = nullptr;
 		bool file_exists(const std::string& file_name) {
 			ifstream test_file {"../config/config.txt"};
 			return test_file.good();
